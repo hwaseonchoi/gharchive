03c4985a310277b5574319fd0dac2a94bd1ae0aa
hwa-seon choi <hwaseon.lecouey@gmail.com>
2020-05-14 21:00:28 +0200

    Improve data import script

--- docker/mongo/shared/process.js
+++ docker/mongo/shared/process.js
@@ -1,22 +1,24 @@
-// import des messages de commits dans la collection gharchive
+// import des messages de commits dans la collection gharchive_increment
 db.gharchive_raw.aggregate(
     {
         "$unwind": "$payload.commits"
     },
     {
         "$project": {
-            _id: 0,
+            _id: "$payload.commits.sha", // identifie l'élément
             "type" : 1,
             "actor_login" : "$actor.login",
             "repo_name" : "$repo.name",
             "text" : "$payload.commits.message",
-            "text_type" : "commit"
+            "text_type" : "commit" // type de message
         }
     },
     {
-        "$out": "gharchive"
+        "$merge": { into: "gharchive", on: "_id", whenMatched: "keepExisting", whenNotMatched: "insert" }
     }
 );
+
+
 
 // import des messages de comments dans la collection gharchive
 // db.gharchive_raw.aggregate(
@@ -34,7 +36,7 @@ db.gharchive_raw.aggregate(
 //         }
 //     },
 //     {
-//         $out: "gharchive"
+//         $out: "gharchive_increment"
 //     }
 // )
 
--- import-gh-archive.sh
+++ import-gh-archive.sh
@@ -3,6 +3,11 @@
 set -e
 
 FILE=docker/mongo/shared/$(date -v -1y '+%Y-%m-%d')-10.json
+GREEN="\033[0;32m"
+NOCOLOR="\033[0m"
+echo
+echo -e "${GREEN}>>> Retrieving $FILE from GH Archive...${NOCOLOR}"
+echo
 
 if [ ! -f "$FILE" ]; then
 	cd docker/mongo/shared
@@ -12,11 +17,23 @@ else
 	echo "$FILE" exists already
 fi
 
+echo
+echo -e "${GREEN}>>> Initialising the MongoDB collections...${NOCOLOR}"
+echo
+
 docker exec -w /shared mongo sh -c "/usr/bin/mongo -u root -p rootpassword < init.js"
+
+echo
+echo -e "${GREEN}>>> Importing raw data into MongoDB...${NOCOLOR}"
+echo
 
 docker exec -w /shared mongo /usr/bin/mongoimport -u root -p rootpassword \
     --authenticationDatabase admin \
     --db test --collection gharchive_raw \
     --file $(date -v -1y '+%Y-%m-%d')-10.json
+
+echo
+echo -e "${GREEN}>>> Processing raw data into gharchive collection...${NOCOLOR}"
+echo
 
 docker exec -w /shared mongo sh -c "/usr/bin/mongo -u root -p rootpassword < process.js"
