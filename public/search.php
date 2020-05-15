05b6e4e909fee65440f56de5edc56993c4a59de8
hwa-seon choi <hwaseon.lecouey@gmail.com>
2020-05-15 17:09:24 +0200

    Complete native php script to test and delete Doctrine bundle

--- public/search.php
+++ public/search.php
@@ -1,50 +1,63 @@
+<?php
+
+require '../vendor/autoload.php';
+$searchTerm = $_GET['q'] ?? null;
+
+?>
+
 <html>
 <header>
     <title>GH Archive Search</title>
 </header>
 <body>
+<form>
+    <input type="text" name="q" value="<?php echo $searchTerm; ?>"/>
+    <button type="submit">Search</button>
+</form>
 
 <?php
-require '../vendor/autoload.php';
 
-$m = new MongoDB\Client("mongodb://root:rootpassword@mongo:27017");
+if (null !== $searchTerm) {
 
-$db = $m->test;
-$coll = $db->gharchive;
+$mongoDB = new MongoDB\Client("mongodb://root:rootpassword@mongo:27017");
 
-$searchTerm = $_GET['q'];
+$db = $mongoDB->test;
+$collection = $db->gharchive;
 
-$result = $coll->find( [ '$text' => ['$search' => $searchTerm ] ]);
-$count = $coll->countDocuments( [ '$text' => ['$search' => $searchTerm ] ]);
+$result = $collection->find(['$text' => ['$search' => $searchTerm ]]);
+$count = $collection->countDocuments([ '$text' => ['$search' => $searchTerm ]]);
 ?>
-<p>Total number of results: <?php echo $count; ?></p>
-<table>
-    <thead>
-        <tr>
-            <th>id</th>
-            <th>type</th>
-            <th>actor_login</th>
-            <th>repo_name</th>
-            <th>text_type</th>
-            <th>text</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($result as $entry) {
-        echo '<tr>';
-        echo '<td>' . $entry['_id'] . '</td>';
-        echo '<td>' . $entry['type'] . '</td>';
-        echo '<td>' . $entry['actor_login'] . '</td>';
-        echo '<td>' . $entry['repo_name'] . '</td>';
-        echo '<td>' . $entry['text_type'] . '</td>';
-        echo '<td><pre>' . $entry['text'] . '</pre></td>';
-        echo '</tr>';
-    }
+        <p>Total number of results: <?php echo $count; ?></p>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>type</th>
+                    <th>actor_login</th>
+                    <th>repo_name</th>
+                    <th>text_type</th>
+                    <th>text</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($result as $entry) {
+                echo '<tr>';
+                echo '<td>' . $entry['_id'] . '</td>';
+                echo '<td>' . $entry['type'] . '</td>';
+                echo '<td>' . $entry['actor_login'] . '</td>';
+                echo '<td>' . $entry['repo_name'] . '</td>';
+                echo '<td>' . $entry['text_type'] . '</td>';
+                echo '<td><pre>' . $entry['text'] . '</pre></td>';
+                echo '</tr>';
+            }
+            ?>
 
-    ?>
-    </tbody>
-</table>
-</body>
+            </tbody>
+        </table>
+
+<?php } ?>
+
+    </body>
 </html>
 
