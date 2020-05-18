d6521ef3cd31a4f7a644d64b856f953c84fe0d9d
hwa-seon choi <hwaseon.lecouey@gmail.com>
2020-05-18 14:15:58 +0200

    Optimize shell script for three different data files

--- import-gh-archive.sh
+++ import-gh-archive.sh
@@ -2,22 +2,23 @@
 
 set -e
 
-#FILE=docker/mongo/shared/$(date -v -1y '+%Y-%m-%d')-10.json
-#FILE=docker/mongo/shared/$(date -v -1y '+%Y-%m-%d')-11.json
-FILE=docker/mongo/shared/2019-04-01-12.json
+DEST_DIR=docker/mongo/shared
+FILES="2019-04-01-10.json 2019-04-01-11.json 2019-04-01-12.json "
 GREEN="\033[0;32m"
 NOCOLOR="\033[0m"
 echo
 echo -e "${GREEN}>>> Retrieving $FILE from GH Archive...${NOCOLOR}"
 echo
 
-if [ ! -f "$FILE" ]; then
-	cd docker/mongo/shared
-	wget https://data.gharchive.org/2019-04-01-12.json.gz
-	gunzip 2019-04-01-12.json.gz
-else
-	echo "$FILE" exists already
-fi
+cd $DEST_DIR
+for file in $FILES; do
+    if [ ! -f "$file" ]; then
+        wget https://data.gharchive.org/${file}.gz
+        gunzip ${file}.gz
+    else
+        echo ${file} exists
+    fi
+done
 
 echo
 echo -e "${GREEN}>>> Initialising the MongoDB collections...${NOCOLOR}"
@@ -29,10 +30,12 @@ echo
 echo -e "${GREEN}>>> Importing raw data into MongoDB...${NOCOLOR}"
 echo
 
-docker exec -w /shared mongo /usr/bin/mongoimport -u root -p rootpassword \
-    --authenticationDatabase admin \
-    --db test --collection gharchive_raw \
-    --file 2019-04-01-12.json
+for file in $FILES; do
+    docker exec -w /shared mongo /usr/bin/mongoimport -u root -p rootpassword \
+        --authenticationDatabase admin \
+        --db test --collection gharchive_raw \
+        --file ${file}
+done
 
 echo
 echo -e "${GREEN}>>> Processing raw data into gharchive collection...${NOCOLOR}"
